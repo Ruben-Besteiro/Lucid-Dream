feb5569d31126d17ce3544b54d5f2f246ff2a27e
Untar la Manteca <rbesteiro@proton.me>
2025-09-23 18:35:34 +0200

    itbyuctubc

--- Assets/_Scripts/InputScript.cs
+++ Assets/_Scripts/InputScript.cs
@@ -9,13 +9,12 @@ public class Movimiento : MonoBehaviour
 
     [SerializeField] private float fuerzaMov = 40f;
     [SerializeField] private float velocidadMaxima = 10f;
-    [SerializeField] private float fuerzaSalto = 5f;
+    //[SerializeField] private float fuerzaSalto = 10;
 
     private void Awake()
     {
         controls = new _PLAYERACTIONS();
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true; // Evita que el rigidbody se caiga al inclinarse
     }
 
     private void OnEnable()
@@ -46,27 +45,25 @@ public class Movimiento : MonoBehaviour
 
     private void OnJumpPerformed(InputAction.CallbackContext ctx)
     {
-        rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        rb.AddForce(Vector3.up * 1000, ForceMode.Impulse);
     }
 
     private void FixedUpdate()
     {
-        Vector3 vectorMovVerdadero = new Vector3(vectorMov.x, 0f, vectorMov.y).normalized;
+        transform.rotation = Quaternion.Euler(Vector3.up * 10 * Time.deltaTime * Input.mousePosition.x);
+
+        Vector3 vectorMovVerdadero = transform.TransformDirection(new Vector3(vectorMov.x, 0, vectorMov.y).normalized);     // Con TransformDirection pasamos de global a local
 
         if (vectorMovVerdadero != Vector3.zero)         // Esto chequea si estamos llamando a OnMovePerformed
         {
             rb.AddForce(vectorMovVerdadero * fuerzaMov, ForceMode.Acceleration);
         } else
         {
-            rb.linearVelocity -= rb.linearVelocity / 10;        // Si no le damos a nada pierde un 10% de su velocidad por frame
+            rb.linearVelocity -= rb.linearVelocity / 10;        // Si no le damos a nada pierde un 10% de su velocidad por cada actualización de físicas
         }
 
-        // Limitamos velocidad máxima
-        Vector3 velocidadPlano = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-        if (velocidadPlano.magnitude > velocidadMaxima)
-        {
-            Vector3 velocidadLimitada = velocidadPlano.normalized * velocidadMaxima;
-            rb.linearVelocity = new Vector3(velocidadLimitada.x, rb.linearVelocity.y, velocidadLimitada.z);
-        }
+        Mathf.Clamp(rb.linearVelocity.x, -velocidadMaxima, velocidadMaxima);        // Limitamos la velocidad (la velocidad máxima se puede cambiar al hacer acciones)
+        Mathf.Clamp(rb.linearVelocity.y, -velocidadMaxima, velocidadMaxima);
+        Mathf.Clamp(rb.linearVelocity.z, -velocidadMaxima, velocidadMaxima);
     }
 }
